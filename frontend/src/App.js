0c8df95cd062c95a0d8172bc5282b4d96509094d
aksdf <minhhuanhuynh289@gmail.com>
2021-08-27 01:03:14 +0900

    test react app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/hmhuan"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My Github Profile
         </a>
       </header>
     </div>
